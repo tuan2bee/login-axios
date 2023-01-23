a8ac6cd5c2c033c26ba20086bec17a97ca993271
tuan2bee <tuan2bee@gmail.com>
2023-01-23 23:46:20 +0700

    mobile

--- pages/index.jsx
+++ pages/index.jsx
@@ -75,6 +75,18 @@ export default function Example() {
     { name: "Team", category: "team", href: "#", current: false },
   ]);
 
+  const handleChange = (event) => {
+    setTabSelect(event.target.value);
+    handleGetProfileClick(event.target.value);
+    setTabs(
+      tabs.map((p) =>
+        p.category === event.target.value
+          ? { ...p, current: true }
+          : { ...p, current: false }
+      )
+    );
+  };
+
   const [tabSelect, setTabSelect] = useState("nextjs");
 
   const [selectedPerson, setSelectedPerson] = useState();
@@ -464,18 +476,23 @@ export default function Example() {
                 <div className="pt-0 pb-16">
                   <div className="px-4 sm:px-6 md:px-0">
                     <div className="py-0">
-                      <div className="lg:hidden">
+                      <div className="lg:hidden mt-5">
                         <label htmlFor="selected-tab" className="sr-only">
                           Select a tab
                         </label>
                         <select
+                          onChange={handleChange}
                           id="selected-tab"
                           name="selected-tab"
                           className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-purple-500 focus:outline-none focus:ring-purple-500 sm:text-sm"
-                          defaultValue={tabs.find((tab) => tab.current).name}
+                          defaultValue={
+                            tabs.find((tab) => tab.current).category
+                          }
                         >
                           {tabs.map((tab) => (
-                            <option key={tab.name}>{tab.name}</option>
+                            <option value={tab.category} key={tab.category}>
+                              {tab.name}
+                            </option>
                           ))}
                         </select>
                       </div>
